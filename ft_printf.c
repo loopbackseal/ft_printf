43c6b74a9b40958e621c96d3f7d6fdfac1fdd3e1
loopbackseal <young961027@gmail.com>
2022-02-13 19:04:29 +0900

    feat: Change Treat %% for Treat single '%'

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:31:35 by yohwang           #+#    #+#             */
-/*   Updated: 2022/02/13 18:06:32 by yohwang          ###   ########.fr       */
+/*   Updated: 2022/02/13 18:56:23 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,35 @@
 
 int	check_format(int i, va_list ap, const char *s)
 {
-	int	count;
-	int	tmp;
 //	%d
-/*
-	int int_tmp = va_arg(ap, int);
-	ft_putnbr_fd(int_tmp, 1);
-	return(count_len(int_tmp) + 1);
-*/
+	if (s[i] == 'd')
+	{
+		int int_tmp = va_arg(ap, int);
+		ft_putnbr_fd(int_tmp, 1);
+		return(count_len(int_tmp) + 1);
+	}
 //	%c
 /*
-	ft_putchar(va_arg(ap,int),1);
-	return (1);
+	else if (s[i] == 'c')
+	{
+		ft_putchar(va_arg(ap,int),1);
+		return (1);
+	}
 */
 //	%%
-/*
-	count = 1;
-	while (s[count] == '%')
-		count++;
-	tmp = count;
-	while (tmp - 2 >= 0)
+	else if (s[i] == '%')
 	{
 		ft_putchar('%', 1);
-		tmp = tmp - 2;
+		return (1);
 	}
-	return (count);
-*/
 //	%s
 //	%p
 //	%i
 //	%u
 //	%x
 //	%X
-	return (0);
+	else
+		return (0);
 }
 
 int	ft_printf(const char *s, ...)
@@ -57,14 +53,21 @@ int	ft_printf(const char *s, ...)
 	int		i;
 
 	i = 0;
+	flag = -1;
 	len = ft_strlen(s); // %만날때마다 -2, 길이만큼 +
 	va_start(ap, s);
 	while (s[i] != '\0')
 	{
-		if (s[i++] == '%')
-			len = len - 2 + check_format(i++, ap, s);
+		if (s[i] == '%')
+		{
+			flag = check_format(++i, ap, s);
+			len = len - 2 + flag;
+			if (!flag)
+				--i;
+		}
 		else
-			write(1, &s[i++], 1);
+			ft_putchar(s[i],1);
+		i++;
 	}
 	return (len);
 }
@@ -80,7 +83,8 @@ int main()
 	printf("%d, %d \n",b,c);
 */
 //	%%
-// 	printf("%%%\n");
+ 	ft_printf("ft: %%%b\n");
+	printf("pf: %%%b\n");
 
 /*	return value of "%" ==> 0
 	int a = printf("%");
